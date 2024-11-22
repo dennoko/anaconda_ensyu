2991a2bf5a84046b939af80cdee3d17645a4493f
KurumiSashida <k.sashida@third-scope.com>
2024-11-22 07:20:43 +0000

    サスティン時間の取得とモノラルへの変換

--- check_sustain.py
+++ check_sustain.py
@@ -1,11 +1,9 @@
 import numpy as np
 from pydub import AudioSegment
-import math
 
 # ピークから-40dB小さくなるまでの時間
 def get_sustain(samples, sample_rate, max_db, threshold_db=-40):
     max_amplitude = 2147483647
-    # max_amplitude = np.max(np.abs(samples))
     threshold_amplitude = max_amplitude * 10 ** ((threshold_db + max_db) / 20)
     end_index = len(samples)
     for i in range(len(samples)- 1, -1, -1):
@@ -13,5 +11,6 @@ def get_sustain(samples, sample_rate, max_db, threshold_db=-40):
             end_index = i
             break
     
-    end_time_seconds = end_index / sample_rate / 2
+    end_time_seconds = end_index / sample_rate
+
     return end_time_seconds
