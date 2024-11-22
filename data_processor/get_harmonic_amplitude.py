302c5a4d5398c59052bb5c4fed1448ce0baaa21a
dennoko <sd514dennoko@gmail.com>
2024-11-22 12:15:39 +0000

    save

--- data_processor/get_harmonic_amplitude.py
+++ data_processor/get_harmonic_amplitude.py
@@ -2,12 +2,13 @@
 # 引数: 音の波形データ: list, 基本周波数: float
 # 返り値: 音の倍音成分の大きさ db
 import numpy as np
+import math
 
 def get_harmonic_amplitude(sound: list, base_frequency: float) -> float:
     # フーリエ変換で周波数成分を取得
     n = len(sound)
     fft_result = np.fft.fft(sound)
-    fft_magnitude = np.abs(fft_result)
+    fft_magnitude = np.abs(fft_result)[:n//2]
     freps = np.fft.fftfreq(n, d=1.0/44100)[:n//2]
 
     # 倍音の周波数リストを作成
@@ -15,16 +16,16 @@ def get_harmonic_amplitude(sound: list, base_frequency: float) -> float:
     harmonic_num = 1
     harmonic_freq = base_frequency * harmonic_num
 
-    # 可聴域内の倍音成分を取得
-    while harmonic_freq < 20000:
+    # 倍音の周波数が22050Hzを超えるまで繰り返す 
+    while harmonic_freq < 22050:
         # 倍音周波数に最も近いFFT周波数を取得
         closest_freq = np.argmin(np.abs(freps - harmonic_freq))
-        # 振幅をdbで取得
-        magnitude_db = 20 * np.log10(fft_magnitude[closest_freq] + 1e-10) # 0割りを防ぐために微小値を足す
+        # 振幅をdbで取得 音は16bitでサンプリングしているため、max_amplitudeは 32768
+        magnitude_db = 20 * np.log10(fft_magnitude[closest_freq] / 32768)
         harmonics[harmonic_freq] = magnitude_db
 
         # 次の倍音周波数を計算
         harmonic_num += 1
         harmonic_freq = base_frequency * harmonic_num
-
+    
     return harmonics
---
+++ test.py
@@ -0,0 +1,52 @@
+# 関数等のテスト用のファイル
+# 書き方は適当で良い
+
+from data_processor.get_harmonic_amplitude import get_harmonic_amplitude
+import os
+from pydub import AudioSegment
+
+# 音声データを data/processed/GIBSON_SG_SPETIAL/SG_SPETIAL_CENTERから取得
+# フォルダのパス
+data_path = "data/processed/GIBSON_SG_SPECIAL/SG_SPECIAL_CENTER"
+print("test.py: " + data_path)
+# フォルダ内の各ファイルでループを回す
+for root, dirs, files in os.walk(data_path):
+    for filename in files:
+        # ファイルのパス
+        file_path = os.path.join(root, filename)
+        # ファイルネームの末尾の文字列に応じて基本周波数を設定
+        base_frequency = 440
+        if "_12_1" in filename:
+            base_frequency = 329.628 * 2
+        elif "_12_2" in filename:
+            base_frequency = 246.942 * 2
+        elif "_12_3" in filename:
+            base_frequency = 195.998 * 2
+        elif "_12_4" in filename:
+            base_frequency = 146.832 * 2
+        elif "_12_5" in filename:
+            base_frequency = 110.000 * 2
+        elif "_12_6" in filename:
+            base_frequency = 82.407 * 2
+        elif "_1" in filename:
+            base_frequency = 329.628
+        elif "_2" in filename:
+            base_frequency = 246.942
+        elif "_3" in filename:
+            base_frequency = 195.998
+        elif "_4" in filename:
+            base_frequency = 146.832
+        elif "_5" in filename:
+            base_frequency = 110.000
+        elif "_6" in filename:
+            base_frequency = 82.407
+
+        # 音声ファイルを読み込み
+        sound = AudioSegment.from_wav(file_path)
+        # 音声データをリストに変換
+        sound = sound.get_array_of_samples()
+        # 音の倍音成分の大きさをdbで取得
+        harmonics = get_harmonic_amplitude(sound, base_frequency)
+        print(harmonics)
+        break
+    break
