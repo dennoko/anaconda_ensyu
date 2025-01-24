9878f1e9c341fe68a2ec3fa3b487e3f84f6e83c9
KurumiSashida <k.sashida@third-scope.com>
2025-01-24 07:08:10 +0000

    jsonファイル表示修正

--- src/static/js/get_data.js
+++ src/static/js/get_data.js
@@ -1,18 +1,41 @@
-// JSONデータを非同期に読み込む処理
 window.onload = function() {
-    fetch('../static/js/guitars.json')
-        .then(response => response.json()) // JSONをパース
+    fetch('../static/js/guitars.json')  // JSONファイルのパス
+        .then(response => response.json())  // JSONをパース
         .then(jsonData => {
             const outputDiv = document.getElementById('output');
             jsonData.forEach(guitar => {
                 // ギター情報をHTMLに出力
                 const guitarInfo = `
-                    <div>
-                        <h2>${guitar.種類} - ${guitar.ブランド} ${guitar.モデル}</h2>
-                        <p><strong>特徴:</strong></p>
-                        <ul>
-                            ${guitar.特徴.map(feature => `<li>${feature}</li>`).join('')}
-                        </ul>
+                    <div class="guitar">
+                        <div class="guitar-info">
+                            <h2>${guitar["ギター名"]}</h2>
+                            <h2>${guitar["メーカー"]}</h2>
+                            <h2>(${guitar["メーカーの国"]})</h2>
+                        </div>
+                        <div class="guitar-details">
+                            <h3>音のバランスの評価</h3>
+                            <p>${guitar["音のバランスの評価"]}</p>
+                        </div>
+                        <div class="guitar-details">
+                            <h3>サステインの評価</h3>
+                            <p>${guitar["サステインの評価"]}</p>
+                        </div>
+                        <div class="guitar-details">
+                            <h3>レスポンスの評価</h3>
+                            <p>${guitar["レスポンスの評価"]}</p>
+                        </div>
+                        <div class="guitar-details">
+                            <h3>倍音の評価</h3>
+                            <p>${guitar["倍音の評価"]}</p>
+                        </div>
+                        <div class="guitar-details">
+                            <h3>トーンの評価</h3>
+                            <p>${guitar["トーンの評価"]}</p>
+                        </div>
+                        <div class="guitar-details">
+                            <h3>総合評価</h3>
+                            <p>${guitar["総合評価"]}</p>
+                        </div>
                     </div>
                 `;
                 outputDiv.innerHTML += guitarInfo;
